b7a058657764e973d7bde39ad3c133d6f361f93d
Yastrenky <ybramos2014@gmail.com>
2019-08-25 18:56:23 -0400

    added emojis and ajusted mobile responsive

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -13,8 +13,8 @@ class Login extends React.Component {
     super(props);
 
     this.state = {
-      name: "Yastrenky",
-      pass: "zxcvbnm99",
+      name: "",
+      pass: "",
       errorLog: false,
       errorText:""
     }
